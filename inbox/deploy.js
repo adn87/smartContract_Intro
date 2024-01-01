ce49574fdf5329e5cde2c745a5ada2d73d09e41b
adn87 <aaghadieh@gmail.com>
2024-01-01 09:49:58 -0500

    smartContract_Intro_003
    - setting up the wallet provider.

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,10 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile')
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+  'audit pledge alert glove peanut clever balcony shuffle shift narrow piano brave',
+  'https://goerli.infura.io/v3/59628b926dda4de4b677eb5f8b27fc1e'
+);
+
+const web3 = new Web3(provider);
