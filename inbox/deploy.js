8574b1ea05ffd5a986cc5a82c4f53b0be3c256b3
adn87 <aaghadieh@gmail.com>
2024-01-01 10:19:13 -0500

    smartContract_Intro_004
    - code clean up

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,12 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
 //updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile')
 
 // deploy code will go here
+const provider = new HDWalletProvider(
+  '*',
+  '*'
+);
+
+const web3 = new Web3(provider);
